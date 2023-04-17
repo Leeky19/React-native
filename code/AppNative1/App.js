590326b44d453a48805359020eee095452fad91a
Julien <arzourjulien@gmail.com>
2023-04-17 14:23:41 +0200

    creation dossier support

--- code/AppNative1/App.js
+++ code/AppNative1/App.js
@@ -1,10 +1,25 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [enteredTodo, setEnteredTodo]= useState('')
   return (
-    <View style={styles.container}>
-      <Text>Comment ça pas mal le bzez ?</Text>
+    <View style={styles.containerFlex}>
+      <View style={styles.blueFlexItem}>
+      <Text>Blue</Text>
+      </View>
+      <View style={styles.greenFlexItem}>
+      <Text>{enteredTodo}</Text>
+      <TextInput
+        placeholder='Entrer un nouvelle valeur de state'
+      />
+      <Button title ='Creat todo' />
+      </View>
+      <View style={styles.yellowFlexItem} >
+      <Text>Yellow</Text>
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +28,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  containerFlex: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'orange'
+  },
+  blueFlexItem : {
+    flex: 1,
+    backgroundColor: 'blue'
+  },
+  greenFlexItem : {
+    flex: 1,
+    backgroundColor: 'green'
+  },
+  yellowFlexItem : {
+    flex: 1,
+    backgroundColor: 'yellow'
   },
 });
